8730f586795a8604429129f1a776d725c5438619
Otto Bittner <otto-bittner@gmx.de>
2020-05-28 17:45:24 +0200

    Add more info to output of forkserver_test

--- test/forkserver_test.c
+++ test/forkserver_test.c
@@ -6,8 +6,23 @@ int main() {
     printf("HELLO THIS TEST STARTS HERE!!!\n");
 
     char buf[100];
+    puts("pre recv:");
+    char* tmp = getenv("LETS_DO_THE_TIMEWARP_AGAIN");
+    if(tmp){
+        printf("env val: %s\n", tmp);
+    } else{
+        puts("no LDTTA var..");
+    }
+
     recv(100, buf, 200, 0);
 
+    puts("post recv:");
+    tmp = getenv("LETS_DO_THE_TIMEWARP_AGAIN");
+    if(tmp){
+        printf("env val: %s\n", tmp);
+    } else{
+        puts("no LDTTA var..");
+    }
     printf("NEW TEST WHO DIS?\n");
 
     return 0;
