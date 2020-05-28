a7f2a9769f48ca17aaba76407c6ba240e1d3f51c
Otto Bittner <otto-bittner@gmx.de>
2020-05-28 17:44:59 +0200

    Hardcoded version of restore script, need to fix paths in the future

--- restore.sh
+++ restore.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # This file has to write the entire env into the envfile
-echo $1
+env > ./envfile
+echo "1"
+/home/hirnheiner/repos/criu/criu/criu restore -d -vvv -o restore.log --images-dir $1 && echo "OK"
+echo "2"
