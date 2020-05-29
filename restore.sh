264d75649025b465a870a378ae627128b4c3cd5d
Julian <jb@futureplay.de>
2020-05-29 11:39:16 +0200

    make debug prints a bit more clear

--- restore.sh
+++ restore.sh
@@ -2,6 +2,6 @@
 
 # This file has to write the entire env into the envfile
 env > ./envfile
-echo "1"
-/home/hirnheiner/repos/criu/criu/criu restore -d -vvv -o restore.log --images-dir $1 && echo "OK"
-echo "2"
+echo "BASH 1"
+criu restore -d -vvv -o ./restore.log --images-dir $1 && echo "OK"
+echo "BASH 2"
