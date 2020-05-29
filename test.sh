bd27e779f768717812007eac91851ef807184c22
Julian <jb@futureplay.de>
2020-05-29 13:52:05 +0200

    make criu more reliable

--- test.sh
+++ test.sh
@@ -20,7 +20,7 @@ test_restore(){
   mkdir -p /tmp/criu_snapshot
   # This throws a weird error(?) but seems to work:
   # test.sh: line 15: 608344 Killed    setsid stdbuf -oL AFLplusplus/afl-qemu-trace test/forkserver_test < /dev/null &> /tmp/log
-  setsid stdbuf -oL AFLplusplus/afl-qemu-trace test/forkserver_test < /dev/null &> /tmp/log && echo "Initial snap successful" &
+  setsid stdbuf -oL AFLplusplus/afl-qemu-trace test/forkserver_test < /dev/null &> /tmp/log && echo "Initial snap successful"
   unset LETS_DO_THE_TIMEWARP_AGAIN
   sudo rm -f out/* &> /dev/null || (mkdir in out && echo "foo" > in/1) &> /dev/null || true
   sudo -E AFLplusplus/afl-fuzz -i in -o out -m none -r /tmp/criu_snapshot -- sh restore.sh
