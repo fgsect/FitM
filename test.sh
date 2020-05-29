7880764806480ecf81cb06c873701166ab5a94d9
Julian <jb@futureplay.de>
2020-05-29 11:39:24 +0200

    prep afl a bit

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 # Make was annoying with errors/env and I was too tired for learning it atm
 clean(){
   rm -rf envfile
-	unset LETS_DO_THE_TIMEWARP_AGAIN
-	unset TESTENV
-	unset AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES
-	unset AFL_SKIP_CPUFREQ
-	unset AFL_DEBUG_CHILD_OUTPUT
+  unset LETS_DO_THE_TIMEWARP_AGAIN
+  unset TESTENV
+  unset AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES
+  unset AFL_SKIP_CPUFREQ
+  unset AFL_DEBUG_CHILD_OUTPUT
 
-	rm -rf /tmp/criu_snapshot/
-	rm -rf /tmp/log
+  rm -rf /tmp/criu_snapshot/
+  rm -rf /tmp/log
 }
 
 test_restore(){
@@ -22,6 +22,7 @@ test_restore(){
   # test.sh: line 15: 608344 Killed    setsid stdbuf -oL AFLplusplus/afl-qemu-trace test/forkserver_test < /dev/null &> /tmp/log
   setsid stdbuf -oL AFLplusplus/afl-qemu-trace test/forkserver_test < /dev/null &> /tmp/log && echo "Initial snap successful" &
   unset LETS_DO_THE_TIMEWARP_AGAIN
+  sudo rm -f out/* &> /dev/null || (mkdir in out && echo "foo" > in/1) &> /dev/null || true
   sudo -E AFLplusplus/afl-fuzz -i in -o out -m none -r /tmp/criu_snapshot -- sh restore.sh
 }
 
