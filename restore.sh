515e7fe7c9940892604e9db246265f3d44d263d8
Julian <jb@futureplay.de>
2020-06-04 17:05:36 +0200

    the restore script now uses the state path correctly

--- restore.sh
+++ restore.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # This file has to write the entire env into the envfile
-env > ./envfile
 
 # TODO: Clean this up, this looks/is ugly
-PIPE1=$(cat ./log | cut -d$'\n' -f1)
-PIPE2=$(cat ./log | cut -d$'\n' -f2)
+STATE=$(echo $1 | cut -d'/' -f2)
 
-/home/hirnheiner/repos/criu/criu/criu restore -d \
+cd states/$STATE
+env > ./envfile
+PIPE1=$(cat ./stdout | cut -d$'\n' -f1)
+PIPE2=$(cat ./stdout | cut -d$'\n' -f2)
+
+criu restore -d \
     -vvv \
     -o ./restore.log \
-    --images-dir $1 \
+    --images-dir ./snapshot \
     --inherit-fd "fd[198]:$PIPE1" \
     --inherit-fd "fd[199]:$PIPE2" \
     && echo "OK"
