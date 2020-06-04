ae89e2b6b8c88d21b5841046e2f21e67012b981e
Otto Bittner <otto-bittner@gmx.de>
2020-06-04 09:35:58 +0200

    Fix restore script to inherit correct fds
    Co-authored-by: Julian <jb@futureplay.de>

--- restore.sh
+++ restore.sh
@@ -2,16 +2,15 @@
 
 # This file has to write the entire env into the envfile
 env > ./envfile
-echo "BASH 1"
-PIPE=$(cat /tmp/log | cut -d$'\n' -f1)
-# exec 188</proc/self/fd/198
-# exec 189>/proc/self/fd/199
+
+# TODO: Clean this up, this looks/is ugly
+PIPE1=$(cat /tmp/log | cut -d$'\n' -f1)
+PIPE2=$(cat /tmp/log | cut -d$'\n' -f2)
+
 /home/hirnheiner/repos/criu/criu/criu restore -d \
     -vvv \
     -o ./restore.log \
     --images-dir $1 \
-    --inherit-fd "fd[198]:$PIPE fd[199]:$PIPE" \
+    --inherit-fd "fd[198]:$PIPE1" \
+    --inherit-fd "fd[199]:$PIPE2" \
     && echo "OK"
-echo -n "restore.sh still has the following fds: "
-ls -la /proc/$$/fd
-echo "BASH 2"
