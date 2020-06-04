7f898120aa9d9c09929236b98757401a45e0254f
Julian <jb@futureplay.de>
2020-06-04 17:04:30 +0200

    error handling the second

--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,5 @@ fn main() {
 
     println!("Welcome to FitM!");
 
-    if let Err(e) = fitm::run() {
-        eprintln!("Application error: {}", e);
-
-        process::exit(1);
-    }
+    fitm::run();
 }
