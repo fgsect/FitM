1e59b32955b90f6597a1d89ecd26c2607b2d7192
Otto Bittner <otto-bittner@gmx.de>
2020-06-13 16:54:18 +0200

    C&P super simple c client for testing
    Taken from: https://www.geeksforgeeks.org/socket-programming-cc/

---
+++ test/pseudoclient.c
@@ -0,0 +1,42 @@
+
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 8080
+
+int main()
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *msg = "R";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address / Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , msg , strlen(msg) , 0 );
+    printf("Client sent: %s\n", msg);
+    read( sock , buffer, 1024);
+    printf("%s\n",buffer );
+    return 0;
+}
