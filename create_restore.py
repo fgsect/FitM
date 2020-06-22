45fa7388b0f42b705c302df7444f46fe9fd83857
Otto Bittner <otto-bittner@gmx.de>
2020-06-22 13:38:26 +0200

    Fix minor typos/bugs
    ca: liikt

--- create_restore.py
+++ create_restore.py
@@ -11,17 +11,17 @@ def main():
 
     # execute from fitm/
 
-    base_state = f"{getcwd}/active-state/{argv[1]}"[1:]
-    cur_state = f"{getcwd}/active-state/{argv[2]}"[1:]
+    base_state = f"{getcwd()}/active-state/{argv[1]}"[1:]
+    cur_state = f"{getcwd()}/active-state/{argv[2]}"[1:]
 
-    lines = [x.strip() for open("./restore.sh.tmp", "r").readlines()]
-    lines.append(f"    --inherit-fd[1]:{base_state}/stdout")
-    lines.append(f"    --inherit-fd[2]:{base_state}/stderr")
+    lines = [x.strip("\n") for x in open("./restore.sh.tmp", "r").readlines()]
+    lines.append(f"    --inherit-fd \"fd[1]:{base_state}/stdout\" \\")
+    lines.append(f"    --inherit-fd \"fd[2]:{base_state}/stderr\" \\")
 
-    call(f"python3 crit decode -i /{base_state}/snapshot/files.img --pretty -o ./file".split())
-    call(f"python3 crit decode -i /{base_state}/snapshot/fdinfo-2.img --pretty -o ./fdinfo".split())
+    call(f"crit decode -i /{base_state}/snapshot/files.img --pretty -o ./file".split())
+    call(f"crit decode -i /{base_state}/snapshot/fdinfo-2.img --pretty -o ./fdinfo".split())
 
-    file_info = json.load(open("./files", "r"))
+    file_info = json.load(open("./file", "r"))
     fd_info = json.load(open("./fdinfo", "r"))
 
     files = filter(lambda x: "reg" in x.keys() and "/fd/" in x["reg"]["name"], file_info["entries"])
@@ -31,14 +31,14 @@ def main():
 
     for f in files:
         fd = list(filter(lambda x: x["id"] == f[0], fd_info["entries"]))[0]
-        mapping.append((fd["id"], f[1]))
+        mapping.append((fd["fd"], f[1]))
 
     for m in mapping:
-        lines.append(f"    --inherit-fd[{m[0]}]:{m[1][1:]}")
+        lines.append(f"    --inherit-fd \"fd[{m[0]}]:{m[1][1:]}\" \\")
 
     lines.append("    && echo 'OK'")
 
-    open("./restore.sh", "w").write("\n".joint(lines))
+    open("./restore.sh", "w").write("\n".join(lines))
 
 if __name__ == "__main__":
     main()
