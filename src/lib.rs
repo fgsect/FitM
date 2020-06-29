81b9342c661022bae8563a93a0ee8ea73a5f3c3e
Otto Bittner <otto-bittner@gmx.de>
2020-06-29 15:49:39 +0200

    Start implementation of create_outputs function
    Also split stdout and stdout-afl
    
    ca: liikt

--- src/lib.rs
+++ src/lib.rs
@@ -102,11 +102,11 @@ impl fmt::Debug for AFLRun {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("AFLRun")
             .field("state_path", &self.state_path)
-            .field("target_bin", &self.target_bin)
-            .field("previous_state_path", &self.previous_state_path)
-            .field("timeout", &self.timeout)
-            .field("server", &self.server)
-            .field("initial", &self.initial)
+            // .field("target_bin", &self.target_bin)
+            // .field("previous_state_path", &self.previous_state_path)
+            // .field("timeout", &self.timeout)
+            // .field("server", &self.server)
+            // .field("initial", &self.initial)
             .finish()
     }
 }
@@ -279,8 +279,10 @@ impl AFLRun {
 
         let stdin_file = fs::File::open(stdin.clone()).unwrap();
         // Open a file for stdout and stderr to log to
-        let stdout = fs::File::create("stdout").unwrap();
-        let stderr = fs::File::create("stderr").unwrap();
+        let stdout = fs::File::create("stdout-afl").unwrap();
+        let stderr = fs::File::create("stderr-afl").unwrap();
+        fs::File::create("stdout").unwrap();
+        fs::File::create("stderr").unwrap();
 
         // Start the initial snapshot run. We use our patched qemu to emulate
         // until the first recv of the target is hit. We have to use setsid to
@@ -317,11 +319,20 @@ impl AFLRun {
     /// Start a single fuzz run in afl which gets restored from an earlier
     /// snapshot. Because we use sh and the restore script we have to skip the
     /// bin check
-    fn fuzz_run(&self) -> io::Result<Child> {
+    fn fuzz_run(&self) -> () {
         // If not currently needed, all states should reside in `saved-state`.
         // Thus they need to be copied to be fuzzed
         copy(
             format!("./saved-states/{}", self.state_path),
+            String::from("./active-state/"),
+        );
+
+        copy(
+            String::from("./saved-states/fitm-c1s0"),
+            String::from("./active-state/"),
+        );
+        copy(
+            String::from("./saved-states/fitm-c0s1"),
             String::from("./active-state/"),
         );
 
@@ -338,13 +349,15 @@ impl AFLRun {
             .unwrap();
 
         // Open a file for stdout and stderr to log to
+        let stdout = fs::File::create("stdout-afl").unwrap();
+        let stderr = fs::File::create("stderr-afl").unwrap();
         fs::File::create("stdout").unwrap();
         fs::File::create("stderr").unwrap();
 
         // Spawn the afl run in a command. This run is relative to the state dir
         // meaning we already are inside the directory. This prevents us from
         // accidentally using different resources than we expect.
-        let ret = Command::new("../../AFLplusplus/afl-fuzz")
+        Command::new("../../AFLplusplus/afl-fuzz")
             .args(&[
                 format!("-i"),
                 format!("./in"),
@@ -361,15 +374,66 @@ impl AFLRun {
                 format!("{}", self.state_path),
                 format!("@@"),
             ])
+            .stdout(Stdio::from(stdout))
+            .stderr(Stdio::from(stderr))
             .env("CRIU_SNAPSHOT_DIR", "./snapshot")
             .env("AFL_SKIP_BIN_CHECK", "1")
             .env("AFL_NO_UI", "1")
-            .spawn();
+            .spawn()
+            .expect("[!] Error while spawning fuzz run")
+            .wait()
+            .expect("[!] Error while waiting for fuzz run");
 
         // After spawning the run we go back into the base directory
         env::set_current_dir(&Path::new("../../")).unwrap();
 
-        ret
+        println!("==== [*] Generating outputs for: {} ====", self.state_path);
+
+        self.create_outputs();
+    }
+
+    fn create_outputs(&self) -> () {
+        // Change into our state directory and create the snapshot from there
+        env::set_current_dir(format!("./active-state/{}", self.state_path))
+            .unwrap();
+        let foo = env::current_dir().unwrap();
+        println!("cur dir: {:?}", foo);
+        // Open a file for stdout and stderr to log to
+        let stdout = fs::File::create("stdout-afl").unwrap();
+        let stderr = fs::File::create("stderr-afl").unwrap();
+        fs::File::create("stdout").unwrap();
+        fs::File::create("stderr").unwrap();
+
+        for entry in fs::read_dir("./out/queue").expect(&format!(
+            "[!] Could not read queue of state: {}",
+            self.state_path
+        )) {
+            let entry_path = entry.unwrap().path();
+            let entry_file = fs::File::open(entry_path.clone())
+                .expect("[!] Could not open queue file");
+
+            let _ = Command::new("setsid")
+                .args(&[
+                    format!("stdbuf"),
+                    format!("-oL"),
+                    format!("../../restore.sh"),
+                    format!("{}", self.state_path),
+                    String::from(entry_path.clone().to_str().unwrap()),
+                ])
+                .stdin(Stdio::from(entry_file))
+                .stdout(Stdio::from(stdout.try_clone().unwrap()))
+                .stderr(Stdio::from(stderr.try_clone().unwrap()))
+                .env("FITM_CREATE_OUTPUTS", "1")
+                .env("CRIU_SNAPSHOT_DIR", "./snapshot")
+                .env("AFL_NO_UI", "1")
+                .spawn()
+                .expect("[!] Could not spawn snapshot run")
+                .wait()
+                .expect("[!] Snapshot run failed");
+        }
+
+        // After spawning the run we go back into the base directory
+        env::set_current_dir(&Path::new("../../")).unwrap();
     }
 
     /// Generate the maps provided by afl-showmap. This is used to filter out
@@ -395,6 +459,8 @@ impl AFLRun {
             .unwrap();
 
         // Open a file for stdout and stderr to log to
+        let stdout = fs::File::create("stdout-afl").unwrap();
+        let stderr = fs::File::create("stderr-afl").unwrap();
         fs::File::create("stdout").unwrap();
         fs::File::create("stderr").unwrap();
 
@@ -419,6 +485,8 @@ impl AFLRun {
                 format!("{}", self.previous_state_path),
                 format!("@@"),
             ])
+            .stdout(Stdio::from(stdout))
+            .stderr(Stdio::from(stderr))
             .env("CRIU_SNAPSHOT_DIR", "./snapshot") // which folder a snapshot will be saved to
             .env("AFL_SKIP_BIN_CHECK", "1")
             .env("AFL_NO_UI", "1")
@@ -528,9 +596,10 @@ pub fn run() {
     // this does not terminate atm as consolidate_poc does not yet minimize
     // anything
     while !queue.is_empty() {
+        println!("QUEUE: {:?}", queue);
         // kick off new run
         let afl_current = queue.pop_front().unwrap();
-        
+
         if !afl_current.initial {
             //clean active-state dir before doing anything
             std::fs::remove_dir_all("./active-state")
@@ -542,13 +611,7 @@ pub fn run() {
                 "==== [*] Starting the fuzz run of: {} ====",
                 afl_current.state_path
             );
-            let mut child_fuzz = afl_current
-                .fuzz_run()
-                .expect("[!] Failed to start fuzz run");
-
-            child_fuzz
-                .wait()
-                .expect("[!] Error while waiting for fuzz run");
+            afl_current.fuzz_run()
         }
 
         // TODO: Fancier solution? Is this correct?
