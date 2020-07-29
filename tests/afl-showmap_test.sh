78059cf96863514b7218f6598c4b8d73b57a6794
Otto Bittner <otto-bittner@gmx.de>
2020-07-29 19:31:09 +0200

    Read test files that went missing with merging

---
+++ tests/afl-showmap_test.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/sh
+# Make was annoying with errors/env and I was too tired for learning it atm
+clean(){
+  rm -rf envfile
+  unset LETS_DO_THE_TIMEWARP_AGAIN
+  unset TESTENV
+  unset AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES
+  unset AFL_SKIP_CPUFREQ
+  unset AFL_DEBUG_CHILD_OUTPUT
+
+  rm -rf /tmp/criu_snapshot/
+  rm -rf /tmp/log
+  rm -rf states/test
+}
+
+create_snap(){
+  export LETS_DO_THE_TIMEWARP_AGAIN=1
+  export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+  export AFL_SKIP_CPUFREQ=1
+  export AFL_DEBUG_CHILD_OUTPUT=1
+  export AFL_SKIP_BIN_CHECK=1
+
+  old_pwd=$PWD
+  state_dir=$(pwd)/states/test
+  export CRIU_SNAPSHOT_DIR=$state_dir/snapshot
+  mkdir -p $CRIU_SNAPSHOT_DIR
+  cd $state_dir
+  mkdir out
+  mkdir fd
+  touch out/.cur_input
+  chmod 600 out/.cur_input
+  touch stderr
+  touch stdout
+  # This throws a weird error(?) but seems to work:
+  # test.sh: line 15: 608344 Killed    setsid stdbuf -oL AFLplusplus/afl-qemu-trace test/pseudoserver < /dev/null &> /tmp/log
+  setsid stdbuf -oL ../../AFLplusplus/afl-qemu-trace ../../test/pseudoserver < /dev/null 1> ./stdout 2> ./stderr && echo "Initial snap successful"
+}
+
+fuzz_snap(){
+  unset LETS_DO_THE_TIMEWARP_AGAIN
+  sudo rm -f out/* &> /dev/null || echo "rm failed"
+  mkdir -p "in" "out" "fd" &> /dev/null || echo "mkdir failed"
+  echo "RI" > "in/foobar"
+  echo "supertest" > "in/eintest"
+  backup_snap
+  sudo -E ../../AFLplusplus/afl-showmap -i ./in -o ./out -m none -Q -- sh ../../restore.sh ./test @@
+}
+
+backup_snap(){
+  sudo rm -rf /tmp/test
+  sudo cp -r $state_dir /tmp/test
+}
+
+clean
+create_snap
+fuzz_snap
---
+++ tests/self-dump_test.c
@@ -0,0 +1,118 @@
+//
+// Created by hirnheiner on 11.05.20.
+// Checkout the Makefile
+
+#include "rpc.pb-c.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_MSG_SIZE 1024
+
+static int send_req(int socket_fd, CriuReq *req)
+{
+    unsigned char buf[MAX_MSG_SIZE];
+    int len;
+
+    len = criu_req__get_packed_size(req);
+
+    if (criu_req__pack(req, buf) != len) {
+        perror("Failed packing request");
+        return -1;
+    }
+
+    if (write(socket_fd, buf, len)  == -1) {
+        perror("Can't send request");
+        return -1;
+    }
+
+    return 0;
+}
+
+int do_criu(){
+    CriuReq req		= CRIU_REQ__INIT;
+    CriuResp *resp		= NULL;
+    int fd, dir_fd;
+    int ret = 0;
+    struct sockaddr_un addr;
+    socklen_t addr_len;
+
+    dir_fd = open("/tmp/criu_snapshot", O_DIRECTORY);
+    if (dir_fd == -1) {
+        perror("Can't open /tmp/criu_snapshot dir");
+        return -1;
+    }
+
+    req.type			= CRIU_REQ_TYPE__DUMP;
+    req.opts			= malloc(sizeof(CriuOpts));
+    if (!req.opts) {
+        perror("Can't allocate memory for dump request");
+        return -1;
+    }
+
+    criu_opts__init(req.opts);
+    req.opts->images_dir_fd		= dir_fd;
+    req.opts->log_level		= 4;
+    req.opts->shell_job = 1;
+
+    fd = socket(AF_LOCAL, SOCK_SEQPACKET, 0);
+    if (fd == -1) {
+        perror("Can't create socket");
+        return -1;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_LOCAL;
+
+    strcpy(addr.sun_path, "/tmp/criu_service.socket");
+
+    addr_len = strlen(addr.sun_path) + sizeof(addr.sun_family);
+
+    ret = connect(fd, (struct sockaddr *) &addr, addr_len);
+    if (ret == -1) {
+        perror("Can't connect to socket");
+        goto exit;
+    }
+
+    /*
+     * Send request
+     */
+    ret = send_req(fd, &req);
+    if (ret == -1) {
+        perror("Can't send request");
+        goto exit;
+    }
+
+
+exit:
+    // Closing the socket FD before the process is dumped breaks CRIU
+//    close(fd);
+//    close(dir_fd);
+    if (resp)
+        criu_resp__free_unpacked(resp, NULL);
+    return ret;
+}
+
+
+int main(int argc, char *argv[]) {
+    int state = 0;
+    while(state < 2){
+        printf("%i\n", state++);
+        sleep(1);
+    }
+    puts("requesting process dump");
+
+    // request selfdump
+    do_criu();
+
+    puts("continue...");
+    while(state < 10){
+        printf("%i\n", state++);
+        sleep(1);
+    }
+}
