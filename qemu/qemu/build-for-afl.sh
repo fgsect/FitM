0a6cd962a153ac96fb875137dc0bc8c9bf0eeb25
5aint2ero <henrik.nickel@gmail.com>
2021-01-24 15:58:44 +0000

    more yolo build

--- qemu/qemu/build-for-afl.sh
+++ qemu/qemu/build-for-afl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LIBS='-lprotobuf-c -luuid -L ../../../criu/lib/c/ -L ../../../criu/images/ -l:libcriu.so'
+export LIBS='-lprotobuf-c -luuid -L../../../criu/lib/c -l:libcriu.a -L../../criu/lib/c -l:libcriu.a'
 export LDFLAGS="$LIBS"
 export QEMU_LDFLAGS="$LIBS"
 ./configure --disable-system --enable-linux-user --disable-gtk --disable-sdl --disable-vnc \
@@ -18,5 +18,5 @@ export QEMU_LDFLAGS="$LIBS"
             --disable-system --disable-blobs --disable-tools --python=`which python3` \
 	    --extra-ldflags="$LIBS"
 
-make -j$(nproc) LDFAGS="$LIBS"
+make -j$(nproc) QEMU_LDFLAGS="$LIBS" LDFAGS="$LIBS"
 cp ./x86_64-linux-user/qemu-x86_64 ../../fitm-qemu-trace
