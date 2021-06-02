d70d8ea70ee649c3cf19b27430036d7e84b589fb
Dominik Maier <domenukk@gmail.com>
2021-06-02 19:10:33 +0200

    fixed provisioning

--- provision.sh
+++ provision.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
 chown -Rv _apt:root /var/cache/apt/archives/partial/
 chmod -Rv 700 /var/cache/apt/archives/partial/
-apt-get -y install python-ipaddress make protobuf-compiler protobuf-c-compiler protobuf-compiler libprotobuf-c-dev libprotobuf-dev libnet-dev python3-protobuf python3-yaml protobuf-c-compiler gcc binutils pkg-config libbsd-dev libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev
+apt-get -y update && apt-get -y upgrade
+apt-get -y install ntp # get rid of clock-skew in the vm
+apt-get -y install python-ipaddress make protobuf-compiler protobuf-c-compiler protobuf-compiler libprotobuf-c-dev libprotobuf-dev libnet-dev python3-protobuf python3-yaml protobuf-c-compiler gcc g++ binutils pkg-config libbsd-dev libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev ninja-build libglib2.0-dev cmake libcapstone-dev
+sudo -u vagrant -- sh -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
