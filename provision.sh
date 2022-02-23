4cdfe2adee1798c1e8125b579f24f5599029a8b2
Otto Bittner <otto-bittner@gmx.de>
2022-02-23 20:24:38 +0100

    Add some missing deps to provision script

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@ chown -Rv _apt:root /var/cache/apt/archives/partial/
 chmod -Rv 700 /var/cache/apt/archives/partial/
 apt-get -y update && apt-get -y upgrade
 apt-get -y install ntp # get rid of clock-skew in the vm
-apt-get -y install build-essential binutils pkg-config python-ipaddress make protobuf-compiler protobuf-c-compiler protobuf-compiler libprotobuf-c-dev libprotobuf-dev libnet-dev python3-protobuf python3-yaml protobuf-c-compiler libbsd-dev libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev ninja-build libglib2.0-dev cmake libcapstone-dev
+apt-get -y install build-essential binutils pkg-config python-ipaddress make protobuf-compiler protobuf-c-compiler libprotobuf-c-dev libprotobuf-dev libnet-dev python3-protobuf python3-yaml protobuf-c-compiler libbsd-dev libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev ninja-build libglib2.0-dev cmake libcapstone-dev libaio-dev libnftables-dev iproute2
 sudo -u vagrant -- sh -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
 
 # target deps
